61202bcca4016e8dda747970623fada628a5b863
Ashish Bhardwaj <erashish41@gmail.com>
2024-03-19 07:27:23 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+// String, Number, BigInt, Boolean, undefined, null, Symbol, Object
+
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+console.table([age,isLoggedIn,state])
+
+console.table(typeof age)
