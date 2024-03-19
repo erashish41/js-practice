f7453c33c5faae0664894b9ff284d3fd71f8c010
Ashish Bhardwaj <erashish41@gmail.com>
2024-03-19 07:09:53 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 123456
+let accountEmail = "erashish41@gmail.com"
+var accountPassword = "9988"
+accountCity = "Chandigarh"
+let accountState;
+
+// accountId = 7  // not allowed
+
+accountEmail = "ashish.bhardwaj1036@gmail.com"
+accountPassword = "5858"
+accountCity = "Bengaluru"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
