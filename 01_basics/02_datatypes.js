c9f61c0780c4f70dd5dcfc5b443f41d2355ed1e3
Ashish Bhardwaj <erashish41@gmail.com>
2024-03-19 15:01:05 +0000

    datatype added

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,9 +1,10 @@
+// to find the date of person and find the data type
 // String, Number, BigInt, Boolean, undefined, null, Symbol, Object
+
 
 let age = 18;
 let isLoggedIn = false;
 let state;
 
 console.table([age,isLoggedIn,state])
-
 console.table(typeof age)
