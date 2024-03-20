b77f8eb82550f063604a449c44474194c361177b
Ashish Bhardwaj <erashish41@gmail.com>
2024-03-20 16:19:31 +0000

    conversion done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+// As we are playing a Mario game and we have to show the score
+
+let score = "33abc";
+
+console.log(score);
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1;
+// false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => flase
+// "ashish" => true
+
+
+
+// ************* Operations **************
+
+let str1 = "Ashish"
+let str2 = " Bhardwaj"
+
+console.log(str1 + str2);
+
+
+let gameCounter = 100;
+ gameCounter++ ;
+ console.log(gameCounter);
+
+ ++gameCounter;
+ console.log(gameCounter);
