c568bb10848f9a7c89a48207d1a8cd9522769fb8
Ashish Bhardwaj <erashish41@gmail.com>
2024-03-20 17:01:56 +0000

    summary updated

--- 01_basics/04_datatypes_summary.js
+++ 01_basics/04_datatypes_summary.js
@@ -38,3 +38,34 @@ let myObj = {
 let fun = function(){
     console.log("kida ki hal chal");
 }
+
+
+
+
+// ************ Stack and Heap ***********
+
+// Stack (Primitive data types)
+// Heap (Non-Primitive data types)
+
+// Stack
+let myName = "Ashish"
+let anotherName = myName
+
+anotherName = "ashu"
+
+console.log(myName);
+
+
+
+//Heap
+let ashObj = {
+    email: "erashish41@gmail.com",
+    upi: "erashish41@okhdfcbank"
+}
+
+let otherObj = ashObj
+
+otherObj.email = "ashish.google.com"
+
+console.log(ashObj.email);
+console.log(otherObj.email)
