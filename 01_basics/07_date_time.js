d1db56c8fed7feb186b46576981d3bdb736e184c
Ashish Bhardwaj <erashish41@gmail.com>
2024-03-21 17:14:27 +0000

    date and time

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,23 @@
+// ***********  Date  *******
+let myDate = new Date()
+
+// console.log(myDate.toString());
+
+// console.log(myDate.toLocaleString());
+
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+
+let myCreateDate = new Date (2024, 0, 27)
+// console.log(myCreateDate.toDateString());
+
+// console.log(myCreateDate.toLocaleDateString());
+
+let myTimeStamp = Date.now()
+
+//TimeStamp is used to find the winner of the competition
+console.log(myTimeStamp);
+console.log(myDate);
+
+
