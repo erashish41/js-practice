89fcfa9292c941ca26fb0977f12bb7b9deba3074
Ashish Bhardwaj <erashish41@gmail.com>
2024-03-21 15:45:14 +0000

    string done

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,23 @@
+const name1 = "ashish"
+const repoCount = 20
+
+console.log(`Hello my name is ${name1} and my repo count is ${repoCount}`);
+
+
+const gameName = new String (`ashishbhardwaj`)
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(name1.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf(`d`));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+console.log(gameName.slice(2));
+
+
+const newStr = "   Frontend Developer    "
+console.log(newStr.trim());
