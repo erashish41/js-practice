6aa3b5e9fbea62d5d04200e7e633ef36bb0c4ab3
Ashish Bhardwaj <erashish41@gmail.com>
2024-03-26 09:11:55 +0000

    scope done

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,42 @@
+// var c = 10
+
+// if (true){
+//     let a = 20
+//     const b = 30
+//     var c = 50 
+
+//     // console.log(c);
+//  }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// // **********************************************************************************
+// // child function can fetch parent function
+
+// function one (){
+//     const username = "ashish"
+
+//     function two (){
+//         const job = "frontend"
+//         console.log(username);
+//     }
+//     //  console.log(job);
+
+//      two ()
+// }
+// one ()
+
+// ------------------------------------------------------------------
+
+if (true){
+
+    const username = "ashish"
+
+    if(username === "ashish"){
+        const job = " frontend"
+        console.log(username + job);
+    }
+}
