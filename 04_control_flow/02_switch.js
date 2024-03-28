e046509fd1d9102acee715f19e151336f97fd87a
Ashish Bhardwaj <erashish41@gmail.com>
2024-03-28 19:05:09 +0000

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,43 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+// const month = 3     //month is key
+// switch (month) {    
+//     case 1:         //1 is value
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("february");
+//         break;    
+//     case 3:
+//         console.log("march");
+//         break;
+//     default:
+//         console.log("what ever we want to write");
+//         break;
+// }
+// // const month1 = "march"     //month is key
+// // switch (month1) {    
+// //     case "jan":         //1 is value
+// //         console.log("january");
+// //         break;
+// //     case "feb":
+// //         console.log("february");
+// //         break;    
+// //     case "mar":
+// //         console.log("march");
+// //         break;
+// //     default:
+// //         console.log("what ever we want to write");
+// //         break;
+// // }
+
+
