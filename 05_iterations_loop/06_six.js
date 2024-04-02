58c75cd58ffb377e64e41c459cd58fc64a14f8a7
Ashish Bhardwaj <erashish41@gmail.com>
2024-04-02 19:28:31 +0000

    filter

--- 05_iterations_loop/06_six.js
+++ 05_iterations_loop/06_six.js
@@ -7,7 +7,11 @@
 // })
 // // console.log(values);
 
-//filter
-const myNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const newNums = myNums.filter( (num) => num > 4)
+
+
+// //filter
+// const myNums = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const newNums = myNums.filter( (num) => {
+//     return num > 4
+// })
 console.log(newNums);
