70c9db739ce9cf48fdb82ce64eff646e780f9a7a
Ashish Bhardwaj <erashish41@gmail.com>
2024-04-16 18:23:28 +0000

    promises up

--- 08_advance_one/promises.js
+++ 08_advance_one/promises.js
@@ -5,13 +5,38 @@
 // Pomises can be created and consumes
 // new Promise have callback function and callback have callback then again callback
 
+// Promise One
 const promiseOne = new Promise(function(resolve, reject){
     //do an async task           DB cals, cryptograph, network
     setTimeout(function(){
         console.log('Async task is complete');
+        resolve()     // by resolve mehtod we can connect then to resolve
     }, 1000);
 })
-
 promiseOne.then(function(){
     console.log("Promise resolved");
+})
+
+
+// Promise Two
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log("Async task 2 complete");
+        resolve()
+    },1000)
+}).then(function(){
+    console.log("Async 2 resolved");
+})
+
+
+// Promise Three
+// we can pass object, array and function in resolve method and can fetch the data also
+const promiseThree = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        resolve({username: "ashish", email: "erashish41@gmail.com"})
+    },1000)
+})
+
+promiseThree.then(function(e){
+    console.log(e);
 })
